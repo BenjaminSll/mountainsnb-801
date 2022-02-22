e3aed77590a65f5aa1366cd514a869ceda7ce351
Oladele Houessou <oladele.houessou@yahoo.com>
2022-02-22 11:29:05 +0100

    ajout mountains controller

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -1,4 +1,44 @@
 class MountainsController < ApplicationController
+  def index
+    @mountains = Mountain.all
+  end
 
-  def
+  def show
+    @mountain = Mountain.find(params[:id])
+  end
+
+  def new
+    @mountain = Mountain.new
+  end
+
+  def create
+    @mountain = Mountain.new(mountain_params)
+    @mountain.save
+    if @mountain.save
+      redirect_to mountain_path(@mountain)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @mountain = Mountain.find(params[:id])
+  end
+
+  def update
+    @mountain = Mountain.find(params[:id])
+    @mountain.update(params[:mountain])
+  end
+
+  def destroy
+    @mountain = Mountain.find(params[:id])
+    @mountain.destroy
+    redirect_to mountains_path
+  end
+
+  private
+
+  def mountain_params
+    params.require(:mountain).permit(:name, :range, :height, :location, :terrain, :trails, :difficulty, :price)
+  end
 end
