f04ba63bccd29521062d4fe050a2a13da1f311a9
Benjamin Salloum <benjamin.salloum@gmail.com>
2022-02-22 14:35:06 +0100

    halfway to mountain show

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,4 +1,6 @@
 class Mountain < ApplicationRecord
+  has_one_attached :photo
+
   belongs_to :user, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   validates :location, presence: true
