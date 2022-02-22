37e53dbc19299b03f467b6d589c3b3793060a01a
Benjamin Salloum <benjamin.salloum@gmail.com>
2022-02-22 10:25:42 +0100

    database half seeds

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -2,5 +2,5 @@ class Mountain < ApplicationRecord
   belongs_to :user, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   validates :location, presence: true
-  validates :price, presence: true, format: :decimal, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
