1f78509f0563e59ca94e0128d702384a2e74397c
Léo Bufi <bufi.leo@gmail.com>
2022-02-24 13:49:41 +0100

    booking view done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,14 +2,14 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
-    @mountain = Mountains.find(params[:mountain_id])
+    @mountain = Mountain.find(params[:mountain_id])
     @booking.mountain = @mountain
   end
 
   def create
-    @booking = Booking.new(bookings_params)
-    @mountain = Mountain.find(params[:mountain_id])
-    @booking.mountain = @mountain
+    @booking = Booking.new(booking_params)
+    # @mountain = Mountain.find(params[:mountain_id])
+    # @booking.mountain = @mountain
     @booking.user = current_user
     @booking.total_price = booking_price(@booking)
     @booking.save
@@ -18,7 +18,6 @@ class BookingsController < ApplicationController
   end
 
   def accept
-
     @booking = Booking.find(params[:id])
     @booking.update(status: true)
     # redirect_to dashboard_path(active:"bookings")
@@ -27,10 +26,7 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:bookings).permit(:departure, :arrival, :status, :user_id, :booking_id, :total_price)
+    params.require(:booking).permit(:departure, :arrival, :status, :user_id, :booking_id, :total_price)
   end
 
-  def booking_price(booking)
-    (booking.departure - booking.arrival) * booking.mountain.price
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :mountains, except: :index do
-    resources :bookings, only: %i[new create]
+    resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:destroy]
 end
