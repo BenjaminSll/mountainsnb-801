69b758522f7625732e38d488934b759387f8599a
Léo Bufi <bufi.leo@gmail.com>
2022-02-24 18:12:36 +0100

    dashboard_path_ok

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+
+  def dashboard
+    current_user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :mountains, except: :index do
+  # get /users/:id/dashboard, to: 'pages#dashboard'
+  # post '/users/:id/dashboard', to: 'bookings#accept', as: :booked
+
+  get 'dashboard', to: 'pages#dashboard'
+
+  resources :mountains, except: [:index] do
     resources :bookings, only: [:new, :create, :edit, :update]
   end
-  resources :bookings, only: [:destroy]
-  post 'users/:id/dashboard', to: 'bookings#accept', as: :booked
-  get 'users/:id/dashboard', to: 'pages#dashboard'
+  resources :bookings, only: [:destroy] do
+    post :accept
+  end
 end
